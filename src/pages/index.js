95570e77db9d8168f7c4ad0dc4b3a8ca510526ea
jayantNamaste <jayant@namastedev.com>
2025-02-04 20:41:49 +0530

    restructured the files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,59 +1,74 @@
-import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
-import { PROJECTS, STEPS } from '../../utils/constants';
+import { HERO_SECTION_IMAGE, PROJECTS, STEPS } from '../../utils/constants';
+import { ChevronsDown, CircleCheckBig } from 'lucide-react';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <div className="min-h-screen dark:bg-web-app-bg-dark text-gray-900">
 
-      <header className="bg-blue-600 text-white text-center py-20 px-4">
-        <h1 className="text-4xl font-bold">Build Real-World Projects with Step-by-Step Guidance!</h1>
-        <p className="mt-4 text-lg">Get hands-on experience by working on industry-level projects with our detailed documentation.</p>
-        <button className="mt-6 px-6 py-3 bg-white text-blue-600 font-semibold rounded-lg shadow-md hover:bg-gray-200">
-          Explore Projects
-        </button>
-      </header>
+      <section className="mx-auto flex justify-center gap-20 items-center dark:text-white text-center py-10 px-4 w-[98%] lg:w-[90%] lg:mt-14 border border-brand-border-color rounded-lg">
+        <div>
+          <img src={HERO_SECTION_IMAGE} alt="hero-section-image" className='w-[28rem]' />
+        </div>
+        <div className='flex flex-col gap-3'>
+          <div className='text-start text-2xl lg:text-4xl font-inter font-bold space-y-2 lg:space-y-3 dark:text-white'>
+            <p>Build Real World </p>
+            <p>Projects With </p>
+            <p>Step-By-Step Guidance!</p>
+          </div>
 
-      <section className="py-16 px-6 max-w-6xl mx-auto">
-        <h2 className="text-3xl font-semibold text-center mb-8">Choose Your Difficulty Level</h2>
+          <div className='mt-6 space-y-2 text-opacity-50 dark:text-white'>
+              <div className='flex gap-2 items-center'><div><CircleCheckBig size={15} /></div><div>Explore hands-on projects with clear, step-by-step instructions.</div></div>
+              <div className='flex gap-2 items-center'><div><CircleCheckBig size={15} /></div><div>Learn by building and enhance your development skills.</div></div>
+              <div className='flex gap-2 items-center'><div><CircleCheckBig size={15} /></div><div>Start your journey today and bring your ideas to life!</div></div>
+          </div>
+
+          <button className='mt-6 bg-logo-orange hover:bg-logo-orange-clicked px-6 py-3 w-1/3 text-xl text-white rounded-md'>
+            Get Started
+          </button>
+        </div>
+      </section>
+
+      <ChevronsDown size={40} className='w-full mx-auto mt-6 dark:text-white' />
+
+      <div className="py-8 px-6 max-w-6xl mx-auto dark:text-white">
+        <h2 className="text-3xl font-semibold text-center text-brand-heading mb-16">Choose Your Difficulty Level</h2>
         <div className="grid md:grid-cols-3 gap-6">
           {PROJECTS.map((project, index) => (
-            <div key={index} className="bg-white shadow-lg rounded-lg p-6 text-center relative">
+            <a href={project.link} key={index} className="bg-brand-light-background dark:bg-brand-card-bg rounded-lg p-6 text-center relative">
               <div className='relative'>
                 <img 
                   src={project.image} 
                   alt={project.title} 
                   className="w-full h-48 object-cover rounded-md mb-4 transform transition duration-300 hover:scale-105" 
                 />
-                <div className="absolute top-2 left-2 bg-black bg-opacity-75 text-white px-3 py-1 text-sm font-semibold rounded-md">
+                <div className="absolute top-2 left-2 text-white px-3 py-1 text-xl font-semibold rounded-md">
                   {project.level}
                 </div>
               </div>
               <h3 className="text-xl font-semibold">{project.title}</h3>
-              <p className="text-gray-600 mt-2">{project.description}</p>
-            </div>
+              <p className="text-opacity-50 font-normal mt-2">{project.description}</p>
+            </a>
           ))}
         </div>
-      </section>
+      </div>
 
-      <section className="bg-gray-200 py-16 px-6 text-center">
-        <h2 className="text-3xl font-semibold">How It Works</h2>
+      <ChevronsDown size={40} className='w-full mx-auto mt-8 dark:text-white' />
+
+      <div className="py-12 px-6 text-center dark:text-white">
+        <h2 className="text-3xl font-semibold mb-16 text-brand-heading">How It Works</h2>
         <div className="mt-8 grid md:grid-cols-3 gap-8 max-w-5xl mx-auto">
           {STEPS.map((step, index) => (
-            <div key={index} className="bg-white shadow-lg p-6 rounded-lg">
-              <h3 className="text-xl font-semibold">{step.title}</h3>
-              <p className="text-gray-600 mt-2">{step.description}</p>
+            <div key={index} className="bg-brand-light-background dark:bg-brand-card-bg p-6 rounded-lg">
+              <h3 className="text-xl font-semibold text-brand-blue dark:text-brand-sky-blue">{step.title}</h3>
+              <p className=" mt-2 font-normal">{step.description}</p>
             </div>
           ))}
         </div>
-      </section>
+      </div>
 
       <footer className="bg-[#303846] text-white text-center py-6 mt-12">
         <p>&copy; {new Date().getFullYear()} NamasteDev. All rights reserved.</p>
--- utils/constants.js
+++ utils/constants.js
@@ -4,30 +4,46 @@ export const DOCS_PATH = `${BASE_CDN_URL}/docs/`;
 
 export const LOGO_ICON = `${BANNER_AND_LOGOS_PATH}icon.webp`;
 export const LOGO_NAME = `${BANNER_AND_LOGOS_PATH}name.webp`;
+export const HERO_SECTION_IMAGE = `${DOCS_PATH}docs_hero_image.png`;
 
 export const PROJECTS = [
-    {
-      title: "Beginner Projects",
-      level: "Beginner",
-      description: "Simple and easy-to-build projects to get started with coding.",
-      image: `${DOCS_PATH}beginner.png`
-    },
-    {
-      title: "Intermediate Projects",
-      level: "Intermediate",
-      description: "More complex projects with API integration and real-world use cases.",
-      image: `${DOCS_PATH}intermediate.png`
-    },
-    {
-      title: "Advanced Projects",
-      level: "Advanced",
-      description: "Full-stack and scalable applications with advanced features.",
-      image: `${DOCS_PATH}advanced.png`
-    }
-  ];
-  
-export  const STEPS = [
-    { title: "Choose a Project", description: "Browse through a variety of projects to get started." },
-    { title: "Follow the Guide", description: "Get a structured step-by-step guide to build the project." },
-    { title: "Deploy & Showcase", description: "Complete your project and add it to your portfolio." }
-  ];
+  {
+    title: "Beginner Projects",
+    level: "Beginner",
+    description:
+      "Simple and easy-to-build projects to get started with coding.",
+    image: `${DOCS_PATH}beginner.png`,
+    link: "docs/category/beginner-projects"
+  },
+  {
+    title: "Intermediate Projects",
+    level: "Intermediate",
+    description:
+      "More complex projects with API integration and real-world use cases.",
+    image: `${DOCS_PATH}intermediate.png`,
+    link: "docs/category/intermediate-projects"
+  },
+  {
+    title: "Advanced Projects",
+    level: "Advanced",
+    description: "Full-stack and scalable applications with advanced features.",
+    image: `${DOCS_PATH}advanced.png`,
+    link: "docs/category/advance-projects"
+  },
+];
+
+export const STEPS = [
+  {
+    title: "Choose a Project",
+    description: "Browse through a variety of projects to get started.",
+    icon: ""
+  },
+  {
+    title: "Follow the Guide",
+    description: "Get a structured step-by-step guide to build the project.",
+  },
+  {
+    title: "Deploy & Showcase",
+    description: "Complete your project and add it to your portfolio.",
+  },
+];
