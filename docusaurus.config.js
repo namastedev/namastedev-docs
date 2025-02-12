0ab818c04da4118e678c74f0ab8f7cb2f0c3bf04
Aman <aman@namastedev.com>
2025-02-12 21:14:58 +0530

    chaned base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   url: "https://namastedev.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/namastedev-docs/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
